379f8181eaccf2dfbb1ede8dd8426aab901f7fa2
josecb0508 <chaconbrian0508@gmail.com>
2024-10-02 19:38:39 -0400

    Add files via upload

--- main/main.cpp
+++ main/main.cpp
@@ -1,25 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <Cat.hpp>
 
-//sfml test
-int main()
+int main() 
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Cat Game");
+    window.setFramerateLimit(60);
 
-    while (window.isOpen())
+    Cat cat("/Cat-tastrofe-Cat-tastrofe/resources/cat.png"); 
+
+    sf::Clock clock;
+
+    while (window.isOpen()) 
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window.pollEvent(event)) 
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) 
+            {
                 window.close();
+            }
         }
 
+        float deltaTime = clock.restart().asSeconds();
+        cat.move(deltaTime); 
         window.clear();
-        window.draw(shape);
+        cat.draw(window);    
         window.display();
     }
-
     return 0;
 }
