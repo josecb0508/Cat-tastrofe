7a8a6f1bae5b9747aa72b11849e736821d19d3eb
josecb0508 <chaconbrian0508@gmail.com>
2024-10-09 21:59:14 -0400

    Add files via upload

--- lib/include/Cat.hpp
+++ lib/include/Cat.hpp
@@ -4,22 +4,28 @@ class Cat
 {
     public:
 
-        Cat(const std::string& spriteSheetPath);
+        Cat(const std::string& spriteSheet);
         void move(float deltaTime);
+        void attack(); 
         void draw(sf::RenderWindow& window);
-
+ 
     private:
-        sf::Texture texture;      
-        sf::Sprite sprite;       
-        sf::Vector2f velocity;    
-        sf::IntRect currentFrame; 
-        float animationTime;      
-        float frameDuration;      
-        int currentRow;           
-        int frameCount;         
-        int frameWidth;          
-        int frameHeight;         
-        bool isMoving;
-        void updateAnimation(float deltaTime);
-        void setDirection(int row);
+        void Animation(float deltaTime);
+        void Direction(int row);
+        void Attack_Cat(float deltaTime);
+        sf::Texture texture_;      
+        sf::Sprite sprite_;       
+        sf::Vector2f velocity_;    
+        sf::IntRect current_frame_; 
+        float animation_time_;      
+        float frame_duration_;      
+        int current_row_;           
+        int frame_count_;         
+        int frame_width_;          
+        int frame_height_;         
+        bool moving_;
+        sf::RectangleShape square_;  
+        bool attacking_;              
+        float attack_duration_;           
+        float attack_timer_;
 };
