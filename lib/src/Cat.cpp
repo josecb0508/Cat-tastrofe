9c26b8280f396b6961d170c22c19f15259ad88a8
josecb0508 <chaconbrian0508@gmail.com>
2024-10-23 23:09:45 -0400

    Add files via upload

--- lib/src/Cat.cpp
+++ lib/src/Cat.cpp
@@ -1,7 +1,7 @@
 #include "Cat.hpp"
 #include "Enemy.hpp"
 
-Cat::Cat(const std::string& sprite_cat, const sf::Vector2f& initialPosition)
+Cat::Cat(const std::string& sprite_cat, const sf::Vector2f& initial_position)
     : velocity_(0, 0),
       animation_time_(0),
       frame_duration_(0.1),  
@@ -9,146 +9,187 @@ Cat::Cat(const std::string& sprite_cat, const sf::Vector2f& initialPosition)
       current_row_(0),        
       moving_(false),      
       attacking_(false),
-      attack_duration_(0.2),  
-      attack_timer_(0.1)       
+      attack_duration_(0.1),  
+      attack_timer_(0)       
 {
     texture_.loadFromFile(sprite_cat);
-    frame_width_ = texture_.getSize().x / frame_count_;
-    frame_height_ = texture_.getSize().y / frame_count_;       
+    frame_width_ = 45;
+    frame_height_ = 45;    
     sprite_.setTexture(texture_);
-    current_frame_ = sf::IntRect(0, 0, 28, 28);
+    current_frame_ = sf::IntRect(0, 0, 45, 45);
     sprite_.setTextureRect(current_frame_);
-    sprite_.setScale(2.5, 2.5); 
-    sprite_.setPosition(initialPosition); 
+    sprite_.setScale(2.5f, 2.5f); 
+    sprite_.setPosition(initial_position); 
 
     square_.setSize(sf::Vector2f(50, 50));
     square_.setFillColor(sf::Color::Red);
 
-    size_ = sf::Vector2i(28, 28);
+    size_ = sf::Vector2i(45, 45);
     bounding_square_.setFillColor(sf::Color::Transparent);
     bounding_square_.setOutlineColor(sf::Color::Transparent);
     bounding_square_.setOutlineThickness(1);
     bounding_square_.setSize(sf::Vector2f(size_.x * sprite_.getScale().x, size_.y * sprite_.getScale().y));
 }   
 
-bool Cat::IsRectContained(const sf::FloatRect& outerRect, const sf::FloatRect& innerRect) {
-    sf::Vector2f topLeft(innerRect.left, innerRect.top);
-    sf::Vector2f topRight(innerRect.left + innerRect.width, innerRect.top);
-    sf::Vector2f bottomLeft(innerRect.left, innerRect.top + innerRect.height);
-    sf::Vector2f bottomRight(innerRect.left + innerRect.width, innerRect.top + innerRect.height);
+bool Cat::IsRectContained(const sf::FloatRect& outer_rect, const sf::FloatRect& inner_rect) {
+    sf::Vector2f top_left(inner_rect.left, inner_rect.top);
+    sf::Vector2f top_right(inner_rect.left + inner_rect.width, inner_rect.top);
+    sf::Vector2f bottom_left(inner_rect.left, inner_rect.top + inner_rect.height);
+    sf::Vector2f bottom_right(inner_rect.left + inner_rect.width, inner_rect.top + inner_rect.height);
 
-    return outerRect.contains(topLeft) &&
-           outerRect.contains(topRight) &&
-           outerRect.contains(bottomLeft) &&
-           outerRect.contains(bottomRight);
+    return outer_rect.contains(top_left) &&
+           outer_rect.contains(top_right) &&
+           outer_rect.contains(bottom_left) &&
+           outer_rect.contains(bottom_right);
 }
 
-void Cat::Move(float deltaTime, const Map& room, Enemy& enemy) {
-    velocity_ = sf::Vector2f(0, 0);
-    moving_ = false;
+void Cat::Move(float delta_time, const Map& room, Enemy& enemy) {
+    if (!attacking_) {
+        velocity_ = sf::Vector2f(0, 0);
+        moving_ = false;
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        velocity_.y = -200;
-        SetDirection(2);
-        moving_ = true;
-    } 
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-        velocity_.y = 200;
-        SetDirection(0);
-        moving_ = true;
-    } 
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        velocity_.x = -200;
-        SetDirection(1);
-        moving_ = true;
-    } 
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        velocity_.x = 200;
-        SetDirection(3);
-        moving_ = true;
-    }
-
-    if (moving_) {
-        
-        sf::Vector2f newPosition = sprite_.getPosition() + (velocity_ * deltaTime);
-
-        sf::FloatRect hitboxBounds = bounding_square_.getGlobalBounds();
-        hitboxBounds.left = newPosition.x; 
-        hitboxBounds.top = newPosition.y;
-
-        sf::FloatRect mapBounds = room.GetBounds();
-
-        if (IsRectContained(mapBounds, hitboxBounds)) {
-
-            sprite_.setPosition(newPosition);
-            last_valid_position_ = newPosition; 
-
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            velocity_.y = -200;
+            SetDirection(4);
+            moving_ = true;
         } 
-       
-        bounding_square_.setPosition(sprite_.getPosition());
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            velocity_.y = 200;
+            SetDirection(0);
+            moving_ = true;
+        } 
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            velocity_.x = -200;
+            SetDirection(2);
+            moving_ = true;
+        } 
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            velocity_.x = 200;
+            SetDirection(6);
+            moving_ = true;
+        }
 
-        Animate(deltaTime);
-    } 
-    else {
-        current_frame_.left = 0; 
-        sprite_.setTextureRect(current_frame_);
+        if (moving_) {
+            sf::Vector2f new_position = sprite_.getPosition() + (velocity_ * delta_time);
+            sf::FloatRect hitbox_bounds = bounding_square_.getGlobalBounds();
+            hitbox_bounds.left = new_position.x; 
+            hitbox_bounds.top = new_position.y;
+
+            sf::FloatRect map_bounds = room.GetBounds();
+            if (IsRectContained(map_bounds, hitbox_bounds)) {
+                sprite_.setPosition(new_position);
+                last_valid_position_ = new_position;
+            } 
+
+            bounding_square_.setPosition(sprite_.getPosition());
+            Animate(delta_time);
+        } else {
+            current_frame_.left = 0;
+            sprite_.setTextureRect(current_frame_);
+        }
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::X) && !attacking_) 
-    {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::X) && !attacking_) {
         StartAttack();
     }
-    Scratch(deltaTime, enemy);    
 
+    Scratch(delta_time, enemy);    
     bounding_square_.setPosition(sprite_.getPosition());
 }
 
-void Cat::Animate(float deltaTime) 
-{
-    animation_time_ += deltaTime;
-    if (animation_time_ >= frame_duration_) 
-    {
+void Cat::Animate(float delta_time) {
+    animation_time_ += delta_time;
+
+    if (attacking_) {
+        attack_timer_ += delta_time;
+
+        if (attack_timer_ >= attack_duration_) {
+            attacking_ = false; 
+            current_row_ = 0;  
+            current_frame_.top = current_row_ * frame_height_;
+            current_frame_.left = 0; 
+        } else if (animation_time_ >= frame_duration_) {
+            animation_time_ = 0;
+            current_frame_.left += frame_width_;
+            if (current_frame_.left >= frame_width_ * 4) {  
+                current_frame_.left = 0;  
+            }
+            sprite_.setTextureRect(current_frame_);
+        }
+    } 
+    else if (animation_time_ >= frame_duration_) {
         animation_time_ = 0;
         current_frame_.left += frame_width_;
-        if (current_frame_.left >= frame_width_ * frame_count_) 
-        {
-            current_frame_.left = 0; 
+        if (current_frame_.left >= frame_width_ * frame_count_) {
+            current_frame_.left = 0;  
         }
-        
-        sprite_.setTextureRect(current_frame_); 
+        sprite_.setTextureRect(current_frame_);
     }
 }
 
-void Cat::StartAttack() 
-{
-    attacking_ = true;
-    attack_timer_ = 0;
+void Cat::StartAttack() {
+    attacking_ = true;   
+    attack_timer_ = 0;   
 
-    if (current_row_ == 0) { 
-        square_.setPosition(sprite_.getPosition().x, sprite_.getPosition().y + 50);
-        square_.setSize(sf::Vector2f(30, 30)); 
-    } else if (current_row_ == 1) { 
-        square_.setPosition(sprite_.getPosition().x - 50, sprite_.getPosition().y);
-        square_.setSize(sf::Vector2f(30, 30));
-    } else if (current_row_ == 2) {  
-        square_.setPosition(sprite_.getPosition().x, sprite_.getPosition().y - 50);
-        square_.setSize(sf::Vector2f(30, 30));
-    } else if (current_row_ == 3) { 
-        square_.setPosition(sprite_.getPosition().x + 50, sprite_.getPosition().y);
-        square_.setSize(sf::Vector2f(30, 30));
+    if (current_row_ == 6) 
+    {       
+        current_row_ = 11;         
+        square_.setSize(sf::Vector2f(20, 60));
+        square_.setPosition(sprite_.getPosition().x + frame_width_ * sprite_.getScale().x - 8, sprite_.getPosition().y + (size_.y / 4) * sprite_.getScale().y);
+    } else if (current_row_ == 2) 
+    { 
+        current_row_ = 9;         
+        square_.setSize(sf::Vector2f(20, 60)); 
+        square_.setPosition(sprite_.getPosition().x - 10, sprite_.getPosition().y + (size_.y / 4) * sprite_.getScale().y);
+    } else if (current_row_ == 0) 
+    {
+        current_row_ = 8;         
+        square_.setSize(sf::Vector2f(50, 20)); 
+        square_.setPosition(sprite_.getPosition().x + (size_.x / 4) * sprite_.getScale().x, sprite_.getPosition().y + frame_height_ * sprite_.getScale().y - 8);
+    } else if (current_row_ == 4) 
+    { 
+        current_row_ = 10;         
+        square_.setSize(sf::Vector2f(50, 20)); 
+        square_.setPosition(sprite_.getPosition().x + (size_.x / 4) * sprite_.getScale().x, sprite_.getPosition().y - 10);
     }
-    square_.setFillColor(sf::Color::Transparent);
-    square_.setOutlineColor(sf::Color::Blue); 
+
+    current_frame_.top = current_row_ * frame_height_;  
+    current_frame_.left = 0;                           
+    sprite_.setTextureRect(current_frame_);  
+    square_.setFillColor(sf::Color::Transparent);  
+    square_.setOutlineColor(sf::Color::Transparent);
     square_.setOutlineThickness(2);
 } 
 
-void Cat::Scratch(float deltaTime, Enemy& enemy) 
-{
+void Cat::Scratch(float delta_time, Enemy& enemy) {
     if (attacking_) {
-        attack_timer_ += deltaTime;
-        if (attack_timer_ >= attack_duration_) {
-            attacking_ = false;  
-        } else {
+        attack_timer_ += delta_time;
+        animation_time_ += delta_time;
+
+        if (animation_time_ >= frame_duration_) {
+            animation_time_ = 0;
+            current_frame_.left += frame_width_; 
+
+            if (current_frame_.left >= frame_width_ * 4) {
+                current_frame_.left = 0;  
+                attacking_ = false;
+
+                if (current_row_ == 9) {        
+                    current_row_ = 2;           
+                } else if (current_row_ == 11) { 
+                    current_row_ = 6;          
+                } else if (current_row_ == 8) {  
+                    current_row_ = 0;           
+                } else if (current_row_ == 10) {
+                    current_row_ = 4;          
+                }
+
+                current_frame_.top = current_row_ * frame_height_;
+                current_frame_.left = 0;
+            }
+
+            sprite_.setTextureRect(current_frame_);
+
             if (square_.getGlobalBounds().intersects(enemy.GetHitbox())) {
                 enemy.TakeDamage(10);  
             }
@@ -156,10 +197,8 @@ void Cat::Scratch(float deltaTime, Enemy& enemy)
     }
 }
 
-void Cat::SetDirection(int row) 
-{
-    if (current_row_ != row) 
-    {
+void Cat::SetDirection(int row) {
+    if (current_row_ != row) {
         current_row_ = row;
         current_frame_.top = current_row_ * frame_height_; 
         current_frame_.left = 0;  
@@ -167,12 +206,10 @@ void Cat::SetDirection(int row)
     }
 }
 
-void Cat::Draw(sf::RenderWindow& window) 
-{
+void Cat::Draw(sf::RenderWindow& window) {
     window.draw(bounding_square_);
     window.draw(sprite_);
-    if (attacking_) 
-    {
+    if (attacking_) {
         window.draw(square_);  
     }    
 }
